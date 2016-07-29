af6b9967bee17d0bdb6f35fda3e24a4dadd07e83
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-07-29 11:12:40 +0530

    Initial Version

---
+++ CSharpConsoleApps/HelloWorld/MainCls.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Personal;
+
+using static System.Console;
+
+namespace HelloWorld
+{
+    public class MainCls
+    {
+
+        #region Variables.
+        private const string HeaderDoubleDash = "======================================================================================================";
+        private const string HeaderSingleDash = "------------------------------------------------------------------------------------------------------";
+        #endregion
+
+        public static void Main(string[] args)
+        {
+            var color = args.Length > 0 ? args[0] : "Cyan";
+            ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color, true);
+
+            var person1 = new Person();
+            DisplayOutput("Person Information:", person1.ToString());
+
+            WriteLine("\n\nPress ENTER key ...");
+            ReadLine();
+        }
+
+        private static void DisplayOutput(string titile, string outputData)
+        {
+            WriteLine(HeaderDoubleDash);
+            WriteLine(titile);
+            WriteLine(HeaderSingleDash);
+            WriteLine(outputData);
+            WriteLine(HeaderDoubleDash);
+        }
+    }
+
+}
---
+++ CSharpConsoleApps/Personal/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Personal
+{
+
+    public class Person
+    {
+        #region Variables.
+        private const int Population = 66000000;
+        private const double Weight = 68.88;
+        private const decimal MonthlySalary = 4.99M;
+        private const string Name = "Shiva Sai";
+        private const char Gender = 'M';
+        private const bool IsMarried = true;
+        private const char NewLine = '\n';
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            var personDetails = new StringBuilder(1024);
+            personDetails.Append($"The Population of UK is {Population:N0}");
+            personDetails.Append(NewLine);
+            personDetails.Append($"Your name is {Name} and you are {Gender}");
+            personDetails.Append(NewLine);
+            personDetails.Append($"You are {Weight} Kgs weight and your salary is {MonthlySalary:C}");
+            personDetails.Append(NewLine);
+            personDetails.Append($"You are {IsMarried} person.");
+
+            return personDetails.ToString();
+        }
+        #endregion
+    }
+
+
+}
